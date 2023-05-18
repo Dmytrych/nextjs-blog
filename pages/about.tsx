c93b508bc237e86d98eda4b9d596176aaa521612
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2023-05-18 09:45:33 +0300

    Initial changes

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Link from "next/link";
 
 export const About = (): JSX.Element => {
-  return (
-    <Layout
-      customMeta={{
-        title: 'About - Hunter Chang',
-      }}
-    >
-      <h1>About Page</h1>
-      <p>Welcome to the about page</p>
-    </Layout>
-  );
+    return (
+        <Layout
+            customMeta={{
+                title: 'About - Хабазня Дмитро',
+            }}
+        >
+            <h1>Контакти</h1>
+            <div className="flex-col">
+                <div><Link href="https://github.com/Dmytrych">GitHub</Link></div>
+                <div><Link href="https://www.linkedin.com/in/dmytro-habaznia-9a251419a/">LinkedIn</Link></div>
+                <div>karambolrul@gmail.com</div>
+            </div>
+        </Layout>
+    );
 };
 
 export default About;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 const { spacing } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {

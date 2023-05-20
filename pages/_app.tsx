a2e50cbf9b1512cc4b98f8bc11bccba7069c447b
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2023-05-20 21:00:55 +0300

    Adsense integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,19 @@ import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import '../styles/globals.css';
+import Script from "next/script";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+        <Head>
+            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6281606897577273" crossOrigin="anonymous" />
+        </Head>
+        <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+            <Component {...pageProps} />
+        </ThemeProvider>
+    </>
   );
 };
 

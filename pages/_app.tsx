15fecf2cc61a1b5b75df36d65b9eb87170b04eac
Dmytrych <55029343+Dmytrych@users.noreply.github.com>
2023-05-22 10:22:59 +0300

    Fixed the info, fixed the google adsense and added new article

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,9 +8,13 @@ import Head from "next/head";
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
-        <Head>
-            <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6281606897577273" crossOrigin="anonymous" />
-        </Head>
+        <Script
+            async
+            id="Adsense-id"
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6281606897577273"
+            crossOrigin="anonymous"
+            strategy="beforeInteractive"
+        />
         <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
             <Component {...pageProps} />
         </ThemeProvider>

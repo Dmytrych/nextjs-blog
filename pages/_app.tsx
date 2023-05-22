0176f2f8a548f885771dde80607106f86f95fec6
Dmytro Habaznia <dmytro.habaznia@sigma.software>
2023-05-22 14:39:41 +0300

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,10 +9,26 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     <>
         <Script
             async
-            id="Adsense-id"
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6281606897577273"
             crossOrigin="anonymous"
             strategy="beforeInteractive"
+        />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-86D2228H6F"
+          strategy="beforeInteractive"
+        />
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+  
+            gtag('config', 'G-86D2228H6F');`,
+          }}
         />
         <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
             <Component {...pageProps} />
